7aa0b2ec225bec108b1ae253cf1ede42dfdc89b1
thornie04 <kimchanthon46@gmail.com>
2026-05-21 11:47:35 +0700

    add basePath and change layout provider

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 const nextConfig = {
+  basePath: "/attendance",
+  assetPrefix: "/attendance",
   turbopack: {
     root: __dirname,
   },
-  images:{
-    domains: ["i.pinimg.com"]
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "your-domain.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/provider/providers.tsx
@@ -0,0 +1,12 @@
+
+"use client";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <TooltipProvider>{children}</TooltipProvider>;
+}
